4c96869320b33324a5434b663ade1d08f2b46402
danoan <danoan2008@gmail.com>
2020-02-12 16:35:00 +0100

    Segmentation experiment

--- lab/experiments/flow/exp-flow.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-PROJECT_PATH=$( cd $SCRIPT_PATH && cd ../../../ && pwd )
-
-APP_FLOW=${PROJECT_PATH}/cmake-build-release/app/graph-flow/graph-flow-app
-APP_SUMMARY_FLOW=${PROJECT_PATH}/ext-projects/cmake-build-release/bin/summary-flow
-
-python3 ${SCRIPT_PATH}/instance-generator/generator.py ${PROJECT_PATH} cmake-build-release ${APP_SUMMARY_FLOW} ${SCRIPT_PATH}/output
---
+++ lab/experiments/flow/flow-best-of-neighborhood/instance-generator/config.py
@@ -0,0 +1,45 @@
+class Param:
+    def __init__(self,name,prefix,type,items,resolver):
+        self.name=name
+        self.prefix=prefix
+        self.type=type
+        self.items=[ {'path':resolver(prefix,v),'value':v} for v in items]
+
+GRID_STEP=[1.0,0.5,0.25]
+SHAPES=["bean","square","flower","triangle","ellipse"]
+RADIUS=[5,9]
+ENERGY=["elastica"]
+LENGTH_PENALIZATION=[0.01]
+ITERATIONS=300
+OPT_BAND=[3]
+NEIGH_SIZE=[2]
+NUM_THREADS=32
+
+
+def resolve_double(prefix,d):
+    return "%s%.5f" % (prefix,d,)
+
+def resolve_int(prefix,i):
+    return "%s%d" % (prefix,i,)
+
+def resolve_std(prefix,s):
+    return "%s%s" % (prefix,s)
+
+def valid_combination(c):
+    shape,radius,energy,opt_band,neigh_size,length_pen,gs = c
+
+    flag=True
+
+    return flag
+
+CONFIG_LIST=[ Param("Shape","","shape",SHAPES,resolve_std),
+              Param("Radius","radius_","radius",RADIUS,resolve_int),
+              Param("Energy","","energy",ENERGY,resolve_std),
+              Param("Opt Band","opt_band_","opt_band",OPT_BAND,resolve_int),
+              Param("Neigh Size","neigh_size_","neigh_size",NEIGH_SIZE,resolve_int),
+              Param("Length Penalization","len_pen_","lenght_pen",LENGTH_PENALIZATION,resolve_double),
+              Param("Grid Step","gs_","grid_step",GRID_STEP,resolve_double)]
+
+
+
+
