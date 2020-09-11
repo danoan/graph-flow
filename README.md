c67299f2fed5e0e401c1c1031a0951d63d7ab7d9
danoan <danoan2008@gmail.com>
2020-09-11 18:15:04 +0200

    interactive-seg and README

---
+++ README.md
@@ -0,0 +1,75 @@
+[![Build Status](https://travis-ci.com/danoan/graph-flow.svg?branch=master)](https://travis-ci.com/danoan/graph-flow)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/6059626d43064254affc2b908bcd86e1)](https://app.codacy.com/manual/danoan/graph-flow?utm_source=github.com&utm_medium=referral&utm_content=danoan/graph-flow&utm_campaign=Badge_Grade_Dashboard)
+
+
+## graph-flow
+ Elastica-driven flow optmized via graph-cut based model. 
+ Its main applications are the **graph-flow-app** 
+ and ***interactive-seg** apps. The 
+ latter integrates all the necessary steps to execute
+ a segmentation regularized by the Elastica energy.
+ 
+ 
+## Dependencies 
+
+[libboost1.66.0-dev](https://www.boost.org/users/history/version_1_66_0.html)
+
+[opencv-3.3.0](https://opencv.org/releases.html)
+
+[eigen-3.36](http://eigen.tuxfamily.org/index.php?title=Main_Page)
+
+[DGtal0.9](https://dgtal.org/download/)
+
+[jinja2.11](https://github.com/pallets/jinja)
+
+## Build and Install
+
+In order to build graph-flow, you are going to need cmake.
+
+```
+cd [GRAPHFLOW_ROOT_FOLDER]
+mkdir build
+cd build
+cmake ..
+make install
+```
+
+There are plenty of configuration parameters, but usually, if 
+all dependencies are installed in their standard
+locations, that should be enough. Otherwise, if errors occur, 
+it is likely that a manual configuration should be
+done. In this scenario, ccmake is quite handy.
+
+## How to use
+
+Build instructions can be found in INSTALL.txt
+
+This source contains eight applications. For detailed description,
+type: [application] -?
+
+We give output examples of the main applications: graph-flow-app and interactive-seg
+
+### graph-flow-app
+
+Evolve the digital curvature flow for a given digital shape.
+
+```
+./shape-flow -Striangle -r15 -h0.1
+./shape-flow -Sflower -r15 -h0.1
+```
+
+<img alt="Triangle flow" src="https://github.com/danoan/graph-flow/blob/master/doc/images/triangle.gif" height="200" />
+<img alt="Flower flow" src="https://github.com/danoan/graph-flow/blob/master/doc/images/flower.gif" height="200" />
+
+
+### interactive-seg
+
+Load a image, select foreground/background seeds and then execute the graph-seg algorithm.
+
+```
+./interactive-seg.sh -I images/coala.jpg output/coala
+```
+<img alt="Coala seeds" src="https://github.com/danoan/graph-flow/blob/master/doc/images/coala-seeds.png" height="200" /> <img alt="Coala segmentation" src="https://github.com/danoan/graph-flow/blob/master/doc/images/coala-seg.png" height="200" />
+
+
+
---
+++ app/interactive-seg.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+PROJECT_FOLDER="$(cd "$SCRIPT_FOLDER" && cd .. && pwd)"
+GRAPH_SEG_BIN="${PROJECT_FOLDER}/cmake-build-release/app/graph-seg"
+
+BTOOLS_BIN="${PROJECT_FOLDER}/ext-projects/cmake-build-release/bin"
+INPUT_IMAGE="${PROJECT_FOLDER}/input/images/coala.jpg"
+
+usage(){ echo "Usage: $0 "
+echo "[-r Ball radius (default: 5)]"
+echo "[-g Data regional term (default: 1.0) ]"
+echo "[-b Data boundary term (default: 2.0) ]"
+echo "[-k Squared curvature weight (default:2.5) ]"
+echo "[-a Length weight (default:0.01) ]"
+echo "[-i Max iterations (default:30) ]"
+echo "[-I INPUT_IMAGE_PATH (default:$INPUT_IMAGE) ]"
+echo " OUTPUT_FOLDER" 1>&2; exit 1;}
+
+r="7"
+g="1.0"
+b="2.0"
+k="2.5"
+a="0.01"
+i="30"
+while getopts ":r:g:b:k:a:i:I:" o; do
+    case "${o}" in
+        r)
+            r=$OPTARG
+            ;;
+        g)
+            g=$OPTARG
+            ;;
+	    b)
+	        b=$OPTARG
+	        ;;
+	    k)
+	        k=$OPTARG
+	        ;;
+	    a)
+	        a=$OPTARG
+	        ;;
+	    i)
+	        i=$OPTARG
+	        ;;
+	    I)
+	        INPUT_IMAGE=$OPTARG
+	        ;;
+        :)
+            echo "Invalid option: $OPTARG requires an argument" 1>&2
+            exit 1;
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
+OUTPUT_FOLDER="$1"
+mkdir -p "$OUTPUT_FOLDER"
+
+if [ -z "$OUTPUT_FOLDER" ]
+then
+    usage
+    exit
+fi
+
+SEED_SELECTOR_APP="${BTOOLS_BIN}/seed-selector"
+GRAB_CUT_APP="${BTOOLS_BIN}/grab-cut"
+GRAPH_SEG_APP="${GRAPH_SEG_BIN}/graph-seg-app"
+
+
+IMAGE_NAME="$(basename $INPUT_IMAGE)"
+IMAGE_NAME="${IMAGE_NAME%%.*}"
+
+SP_OUT="${OUTPUT_FOLDER}/${IMAGE_NAME}"
+mkdir -p "$SP_OUT"
+
+
+echo "Output files in: $SP_OUT"
+echo ""
+
+SEED_SELECTOR_OUT=""
+
+if [ ! -f "${SP_OUT}/mask-bg-0.pgm" ]
+then
+    "${SEED_SELECTOR_APP}" "${INPUT_IMAGE}" "${OUTPUT_FOLDER}" -o
+    if [ $? -eq 0 ]
+    then
+        exit
+    fi
+
+    "${GRAB_CUT_APP}" "${INPUT_IMAGE}" "${SP_OUT}/mask-fg-0.pgm" \
+    "${SP_OUT}/mask-bg-0.pgm" \
+    "${SP_OUT}/gc-object.xml" \
+    -u "${SP_OUT}/mask-pbfg-0.pgm" -s
+
+    "${GRAPH_SEG_APP}" "${SP_OUT}/gc-object.xml" -r"$r" -g"$g" -b"$b" -k"$k" -a"$a" -i"$i" -v -s "${SP_OUT}/graph-seg"
+fi
+
+
+
+while :
+do
+    "${SEED_SELECTOR_APP}" "${INPUT_IMAGE}" "$OUTPUT_FOLDER" \
+    -u "${SP_OUT}/mask-pbfg-0.pgm" \
+    -f "${SP_OUT}/mask-fg-0.pgm" \
+    -b "${SP_OUT}/mask-bg-0.pgm" -o
+
+    if [ $? -eq 0 ]
+    then
+        exit
+    fi
+
+    "${GRAB_CUT_APP}" "${INPUT_IMAGE}" "${SP_OUT}/mask-fg-0.pgm" "${SP_OUT}/mask-bg-0.pgm" "${SP_OUT}/gc-object.xml" \
+    -u "${SP_OUT}/mask-pbfg-0.pgm" -s
+
+    "${GRAPH_SEG_APP}" "${SP_OUT}/gc-object.xml" -r"$r" -g"$g" -b"$b" -k"$k" -a"$a" -i"$i" -v -s "${SP_OUT}/graph-seg"
+done
+
