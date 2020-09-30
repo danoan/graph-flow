9c85ba8232990ef6c626045aff9d91b1792e92a1
danoan <danoan2008@gmail.com>
2020-09-30 13:43:45 +0200

    Save images in png

--- app/graph-seg/main.cpp
+++ app/graph-seg/main.cpp
@@ -93,7 +93,7 @@ int main(int argc, char* argv[])
       }
       case App::GraphSegIteration::Running:{
         if(id.saveAllFigures){
-          Display::saveDigitalSetAsImage(ds,id.outputFolder+"/" + String::nDigitsString(gfIteration.iteration,4) + ".pgm");
+          Display::saveDigitalSetAsImage(ds,id.outputFolder+"/" + String::nDigitsString(gfIteration.iteration,4) + ".png");
         }
 
         if(id.printEnergyValue){
@@ -110,7 +110,7 @@ int main(int argc, char* argv[])
         break;
       }
       case App::GraphSegIteration::End:{
-        Display::saveDigitalSetAsImage(ds,id.outputFolder+"/mask-seg.pgm");
+        Display::saveDigitalSetAsImage(ds,id.outputFolder+"/mask-seg.png");
 
         if(id.displayFlow) {
           cv::Mat bcImage;
--- modules/utils/src/display.cpp
+++ modules/utils/src/display.cpp
@@ -2,20 +2,26 @@
 
 namespace GraphFlow::Utils::Display
 {
-    void saveDigitalSetAsImage(const Domain& domain, const PointSet& ps, const std::string& outputFilepath)
-    {
-        DigitalSet ds(domain);
-        ds.insert(ps.begin(),ps.end());
-        saveDigitalSetAsImage(ds,outputFilepath);
-    }
+void saveDigitalSetAsImage(const Domain& domain, const PointSet& ps, const std::string& outputFilepath)
+{
+  DigitalSet ds(domain);
+  ds.insert(ps.begin(),ps.end());
+  saveDigitalSetAsImage(ds,outputFilepath);
+}
 
-    void saveDigitalSetAsImage(const DigitalSet& ds,const std::string& outputFilepath)
-    {
-        typedef DIPaCUS::Representation::Image2D Image2D;
-        Image2D image(ds.domain());
+void saveDigitalSetAsImage(const DigitalSet& ds,const std::string& outputFilepath)
+{
+  const Domain& domain = ds.domain();
+  const Point& lb = domain.lowerBound();
+  const Point& ub = domain.upperBound();
+  Point dims = ub-lb + Point(1,1);
 
+  cv::Mat cvImgGray = cv::Mat::zeros(dims[1],dims[0],CV_8UC1);
+  DIPaCUS::Representation::digitalSetToCVMat(cvImgGray,ds);
 
-        DIPaCUS::Representation::digitalSetToImage(image,ds);
-        DGtal::GenericWriter<Image2D>::exportFile(outputFilepath,image);
-    }
+  cv::Mat cvImg = cv::Mat::zeros(cvImgGray.rows,cvImgGray.cols,CV_8UC3);
+  cv::cvtColor(cvImgGray,cvImg,cv::COLOR_GRAY2RGB,3);
+
+  cv::imwrite(outputFilepath,cvImgGray);
+}
 }
