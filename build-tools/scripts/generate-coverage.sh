5c31402c5b5950fd65b1297884c1ebf6954accce
DanielMA <danoan2008@gmail.com>
2022-05-29 13:11:23 +0200

    update generate coverage script

--- build-tools/scripts/generate-coverage.sh
+++ build-tools/scripts/generate-coverage.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-SCRIPT_PATH=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
-PROJECT_ROOT=$(cd $SCRIPT_PATH && cd .. && pwd)
-BUILD_FOLDER=${BUILD_DIR}
+SCRIPT_PATH="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
+PROJECT_ROOT="${SCRIPT_PATH%graph-flow/*}graph-flow"
+BUILD_FOLDER="${PROJECT_ROOT}/cmake-build-release"
 
-GCOV_FILES=${BUILD_FOLDER}/lab/test/coverage/CMakeFiles/test-coverage.dir
-COVERAGE_OUTPUT_FOLDER=${PROJECT_ROOT}/lab/test/coverage/output
-CODECOV_OUTPUT_FOLDER=${PROJECT_ROOT}/codecov
+GCOV_FILES="${BUILD_FOLDER}/lab/test/coverage/CMakeFiles/test-coverage.dir"
+COVERAGE_OUTPUT_FOLDER="${PROJECT_ROOT}/lab/test/coverage/output"
+CODECOV_OUTPUT_FOLDER="${PROJECT_ROOT}/codecov"
 
-${BUILD_FOLDER}/lab/test/coverage/test-coverage
 mkdir -p ${COVERAGE_OUTPUT_FOLDER}
+
+GCOV_BUILD="${PROJECT_ROOT}/gcov-build"
+mkdir -p "${GCOV_BUILD}"
+
+pushd "${GCOV_BUILD}" > /dev/null
+gcov "${PROJECT_ROOT}/lab/test/coverage/test-coverage.cpp" --object-directory "${GCOV_FILES}"
 
 lcov --capture --directory ${GCOV_FILES} --output-file ${COVERAGE_OUTPUT_FOLDER}/test_coverage.info
 # genhtml "${COVERAGE_OUTPUT_FOLDER}/test_coverage.info" --output-directory "${COVERAGE_OUTPUT_FOLDER}/report"
 
 bash <(curl -s https://codecov.io/bash)
+
+popd > /dev/null
